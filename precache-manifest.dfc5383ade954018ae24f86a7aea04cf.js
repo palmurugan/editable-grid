b28794495cf80580ce0d19ce4434abba23260865
Palmurugan Chandran <palmuruganc@suntecsbs.com>
2020-07-10 18:38:01 +0530

    Updates

---
+++ precache-manifest.dfc5383ade954018ae24f86a7aea04cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf6070b18539f86260e1020c62ad3809",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f358ab39e1d20cd111ec",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "dd941540f5db2c2e05f0",
+    "url": "./static/js/2.3167661a.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "./static/js/2.3167661a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f358ab39e1d20cd111ec",
+    "url": "./static/js/main.324fb933.chunk.js"
+  },
+  {
+    "revision": "786667bde5f0fdb72b4d",
+    "url": "./static/js/runtime-main.ceb9a8cd.js"
+  }
+]);
